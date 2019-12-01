8506db06ae74ac7bfd994f8c9c847101741be406
Denys Vuika <denys.vuika@gmail.com>
2019-12-01 20:17:07 +0000

    app code enhancements

--- src/app/main.js
+++ src/app/main.js
@@ -1,9 +1,9 @@
 /*global flowy*/
 
 $(document).ready(() => {
-  let rightcard = false;
-  let tempblock;
-  let tempblock2;
+  let rightCard = false;
+  let tempBlock;
+  let tempBlock2;
 
   $('#blocklist').html(
     '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="1"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/eye.svg"></div><div class="blocktext">                        <p class="blocktitle">New visitor</p><p class="blockdesc">Triggers when somebody visits a specified page</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="2"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Action is performed</p><p class="blockdesc">Triggers when somebody performs a specified action</p></div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="3"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/time.svg"></div><div class="blocktext">                        <p class="blocktitle">Time has passed</p><p class="blockdesc">Triggers after a specified amount of time</p>          </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="4"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Error prompt</p><p class="blockdesc">Triggers when a specified error happens</p>              </div></div></div>'
@@ -14,47 +14,50 @@ $(document).ready(() => {
   function snapping(drag) {
     drag.children('.grabme').remove();
     drag.children('.blockin').remove();
-    if (drag.children('.blockelemtype').val() == '1') {
+
+    const blockType = drag.children('.blockelemtype').val();
+
+    if (blockType === '1') {
       drag.append(
         "<div class='blockyleft'><img src='assets/eyeblue.svg'><p class='blockyname'>New visitor</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When a <span>new visitor</span> goes to <span>Site 1</span></div>"
       );
-    } else if (drag.children('.blockelemtype').val() == '2') {
+    } else if (blockType === '2') {
       drag.append(
         "<div class='blockyleft'><img src='assets/actionblue.svg'><p class='blockyname'>Action is performed</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When <span>Action 1</span> is performed</div>"
       );
-    } else if (drag.children('.blockelemtype').val() == '3') {
+    } else if (blockType === '3') {
       drag.append(
         "<div class='blockyleft'><img src='assets/timeblue.svg'><p class='blockyname'>Time has passed</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When <span>10 seconds</span> have passed</div>"
       );
-    } else if (drag.children('.blockelemtype').val() == '4') {
+    } else if (blockType === '4') {
       drag.append(
         "<div class='blockyleft'><img src='assets/errorblue.svg'><p class='blockyname'>Error prompt</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When <span>Error 1</span> is triggered</div>"
       );
-    } else if (drag.children('.blockelemtype').val() == '5') {
+    } else if (blockType === '5') {
       drag.append(
         "<div class='blockyleft'><img src='assets/databaseorange.svg'><p class='blockyname'>New database entry</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Add <span>Data object</span> to <span>Database 1</span></div>"
       );
-    } else if (drag.children('.blockelemtype').val() == '6') {
+    } else if (blockType === '6') {
       drag.append(
         "<div class='blockyleft'><img src='assets/databaseorange.svg'><p class='blockyname'>Update database</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Update <span>Database 1</span></div>"
       );
-    } else if (drag.children('.blockelemtype').val() == '7') {
+    } else if (blockType === '7') {
       drag.append(
         "<div class='blockyleft'><img src='assets/actionorange.svg'><p class='blockyname'>Perform an action</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Perform <span>Action 1</span></div>"
       );
-    } else if (drag.children('.blockelemtype').val() == '8') {
+    } else if (blockType === '8') {
       drag.append(
         "<div class='blockyleft'><img src='assets/twitterorange.svg'><p class='blockyname'>Make a tweet</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Tweet <span>Query 1</span> with the account <span>@DenysVuika</span></div>"
       );
-    } else if (drag.children('.blockelemtype').val() == '9') {
+    } else if (blockType === '9') {
       drag.append(
         "<div class='blockyleft'><img src='assets/logred.svg'><p class='blockyname'>Add new log entry</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Add new <span>success</span> log entry</div>"
       );
-    } else if (drag.children('.blockelemtype').val() == '10') {
+    } else if (blockType === '10') {
       drag.append(
         "<div class='blockyleft'><img src='assets/logred.svg'><p class='blockyname'>Update logs</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Edit <span>Log Entry 1</span></div>"
       );
-    } else if (drag.children('.blockelemtype').val() == '11') {
+    } else if (blockType === '11') {
       drag.append(
         "<div class='blockyleft'><img src='assets/errorred.svg'><p class='blockyname'>Prompt an error</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Trigger <span>Error 1</span></div>"
       );
@@ -63,27 +66,31 @@ $(document).ready(() => {
 
   function drag(block) {
     block.addClass('blockdisabled');
-    tempblock2 = block;
+    tempBlock2 = block;
   }
 
   function release() {
-    tempblock2.removeClass('blockdisabled');
+    tempBlock2.removeClass('blockdisabled');
   }
 
   $(document).on('click', '.navdisabled', function() {
     $('.navactive').addClass('navdisabled');
     $('.navactive').removeClass('navactive');
+
     $(this).addClass('navactive');
     $(this).removeClass('navdisabled');
-    if ($(this).attr('id') == 'triggers') {
+
+    const id = $(this).attr('id');
+
+    if (id === 'triggers') {
       $('#blocklist').html(
         '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="1"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/eye.svg"></div><div class="blocktext">                        <p class="blocktitle">New visitor</p><p class="blockdesc">Triggers when somebody visits a specified page</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="2"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Action is performed</p><p class="blockdesc">Triggers when somebody performs a specified action</p></div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="3"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/time.svg"></div><div class="blocktext">                        <p class="blocktitle">Time has passed</p><p class="blockdesc">Triggers after a specified amount of time</p>          </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="4"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Error prompt</p><p class="blockdesc">Triggers when a specified error happens</p>              </div></div></div>'
       );
-    } else if ($(this).attr('id') == 'actions') {
+    } else if (id === 'actions') {
       $('#blocklist').html(
         '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="5"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/database.svg"></div><div class="blocktext">                        <p class="blocktitle">New database entry</p><p class="blockdesc">Adds a new entry to a specified database</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="6"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/database.svg"></div><div class="blocktext">                        <p class="blocktitle">Update database</p><p class="blockdesc">Edits and deletes database entries and properties</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="7"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Perform an action</p><p class="blockdesc">Performs or edits a specified action</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="8"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/twitter.svg"></div><div class="blocktext">                        <p class="blocktitle">Make a tweet</p><p class="blockdesc">Makes a tweet with a specified query</p>        </div></div></div>'
       );
-    } else if ($(this).attr('id') == 'loggers') {
+    } else if (id === 'loggers') {
       $('#blocklist').html(
         '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="9"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/log.svg"></div><div class="blocktext">                        <p class="blocktitle">Add new log entry</p><p class="blockdesc">Adds a new log entry to this project</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="10"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/log.svg"></div><div class="blocktext">                        <p class="blocktitle">Update logs</p><p class="blockdesc">Edits and deletes log entries in this project</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="11"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Prompt an error</p><p class="blockdesc">Triggers a specified error</p>        </div></div></div>'
       );
@@ -91,13 +98,15 @@ $(document).ready(() => {
   });
 
   $('#close').click(() => {
-    if (rightcard) {
-      rightcard = false;
+    if (rightCard) {
+      rightCard = false;
       $('#properties').removeClass('expanded');
+
       setTimeout(() => {
         $('#propwrap').removeClass('itson');
       }, 300);
-      tempblock.removeClass('selectedblock');
+
+      tempBlock.removeClass('selectedblock');
     }
   });
 
@@ -108,12 +117,12 @@ $(document).ready(() => {
   $(document).on('mousedown', '.block', (/*event*/) => {
     $(document).on('mouseup mousemove', '.block', function handler(event) {
       if (event.type === 'mouseup') {
-        if (!rightcard) {
-          tempblock = $(this);
-          rightcard = true;
+        if (!rightCard) {
+          tempBlock = $(this);
+          rightCard = true;
           $('#properties').addClass('expanded');
           $('#propwrap').addClass('itson');
-          tempblock.addClass('selectedblock');
+          tempBlock.addClass('selectedblock');
         }
       }
       $(document).off('mouseup mousemove', handler);
